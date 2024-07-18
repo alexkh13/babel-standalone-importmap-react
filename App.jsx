8b9c6c8c2abbe675caf771a326a1edf8a15e43d5
alexelalex <67682405+alexelalex@users.noreply.github.com>
2024-07-18 20:41:47 +0300

    initial

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div>This is a test</div>
+    </>
+  );
+}
+
+export default App;
